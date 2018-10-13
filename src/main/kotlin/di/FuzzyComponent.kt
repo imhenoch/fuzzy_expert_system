98ce6173273364bcbee7b33843624541bac4268f
Isidro Henoch <isidro.henoch@gmail.com>
2018-10-13 15:24:40 -0500

    Changes the dagger graph to allow the UI injection

--- src/main/kotlin/di/FuzzyComponent.kt
+++ src/main/kotlin/di/FuzzyComponent.kt
@@ -3,6 +3,9 @@ package di
 import dagger.Component
 import ui.ParentWindow
 import ui.home.HomeUI
+import ui.outputs.OutputsUI
+import ui.range.RangeUI
+import ui.variables.VariablesUI
 
 @Component(modules = [
     UIModule::class
@@ -12,4 +15,10 @@ interface FuzzyComponent {
     fun injectParentWindow(parentWindow: ParentWindow)
 
     fun injectHomeUI(homeUI: HomeUI)
+
+    fun injectVariablesUI(variablesUI: VariablesUI)
+
+    fun injectOutputsUI(outputsUI: OutputsUI)
+
+    fun injectRangeUI(rangeUI: RangeUI)
 }
