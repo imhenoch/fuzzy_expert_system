fbf6ade974a79ab9c5189efb3ec3bee9ddf6eacc
Isidro Henoch <isidro.henoch@gmail.com>
2018-10-14 17:53:01 -0500

    Adds a function that create a new range UI dynamically

--- src/main/kotlin/ui/range/RangeUI.kt
+++ src/main/kotlin/ui/range/RangeUI.kt
@@ -1,18 +1,29 @@
 package ui.range
 
+import files.Register
+import models.Output
+import models.Variable
 import ui.common.Navigation
 import ui.common.UIContainer
 import ui.common.UIForm
 import java.awt.BorderLayout
 import java.awt.FlowLayout
 import javax.inject.Inject
+import javax.swing.Box
 import javax.swing.JButton
 import javax.swing.JComponent
+import javax.swing.JLabel
 import javax.swing.JPanel
+import javax.swing.SwingUtilities
+import javax.swing.border.EmptyBorder
 
 class RangeUI : JPanel(), UIContainer, UIForm {
     @Inject
     lateinit var navigation: Navigation
+    private lateinit var variables: ArrayList<Register<Variable>>
+    private lateinit var outputs: ArrayList<Register<Output>>
+    private val rangesUI by lazy { Box.createVerticalBox() }
+    private val info by lazy { JLabel() }
 
     init {
         setupUI()
@@ -20,22 +31,43 @@ class RangeUI : JPanel(), UIContainer, UIForm {
 
     private fun setupUI() {
         layout = BorderLayout()
+        add(topUI(), BorderLayout.NORTH)
         add(centerUI(), BorderLayout.CENTER)
         add(bottomUI(), BorderLayout.SOUTH)
     }
 
     override fun fetchData() {
-        println("Fetching ranges....")
+        variables = Variable().fetch()
+        outputs = Output().fetch()
+
+        info.text = "Range from 0 to ${variables.size}"
+        SwingUtilities.updateComponentTreeUI(this)
     }
 
     override fun saveData() {
         println("Saving range.....")
     }
 
-    private fun centerUI(): JComponent {
+    private fun topUI(): JComponent {
         val ui = JPanel()
 
-        return ui
+        ui.add(info)
+
+        return ui.apply {
+            border = EmptyBorder(10, 10, 10, 10)
+        }
+    }
+
+    private fun centerUI(): JComponent {
+        generateRangeUI(rangesUI)
+
+        return rangesUI.apply {
+            border = EmptyBorder(10, 10, 10, 10)
+        }
+    }
+
+    private fun generateRangeUI(ui: JComponent) {
+        ui.add(JLabel("Test"))
     }
 
     private fun bottomUI(): JComponent {
