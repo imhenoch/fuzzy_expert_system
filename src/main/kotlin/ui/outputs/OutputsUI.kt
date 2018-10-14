00cef0851998044afeaa54fa3cc6b44c0829481b
Isidro Henoch <isidro.henoch@gmail.com>
2018-10-14 01:38:05 -0500

    Shows the outputs

--- src/main/kotlin/ui/outputs/OutputsUI.kt
+++ src/main/kotlin/ui/outputs/OutputsUI.kt
@@ -4,17 +4,21 @@ import files.Register
 import models.Output
 import ui.common.Navigation
 import ui.common.UIContainer
+import ui.common.chart.ComplexChart
 import java.awt.BorderLayout
 import java.awt.FlowLayout
 import javax.inject.Inject
 import javax.swing.JButton
 import javax.swing.JComponent
 import javax.swing.JPanel
+import javax.swing.SwingUtilities
+import javax.swing.border.EmptyBorder
 
 class OutputsUI : JPanel(), UIContainer {
     @Inject
     lateinit var navigation: Navigation
     private lateinit var outputs: ArrayList<Register<Output>>
+    private val chart by lazy { ComplexChart() }
 
     init {
         setupUI()
@@ -22,11 +26,26 @@ class OutputsUI : JPanel(), UIContainer {
 
     private fun setupUI() {
         layout = BorderLayout()
+        add(centerUI(), BorderLayout.CENTER)
         add(bottomUI(), BorderLayout.SOUTH)
     }
 
     override fun fetchData() {
         outputs = Output().fetch()
+        outputs.forEach { output ->
+            chart.addSeries(output.data.variableName, output.data.points)
+        }
+        SwingUtilities.updateComponentTreeUI(this)
+    }
+
+    private fun centerUI(): JComponent {
+        val ui = JPanel()
+
+        ui.add(chart)
+
+        return ui.apply {
+            border = EmptyBorder(10, 10, 10, 10)
+        }
     }
 
     private fun bottomUI(): JComponent {
