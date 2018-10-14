a4488735dc4615e1572f8b8682d626b09e3e8628
Isidro Henoch <isidro.henoch@gmail.com>
2018-10-13 20:22:06 -0500

    Updates the AddOutputUI to show the chart and updates the ParentWindow size

--- src/main/kotlin/ui/outputs/AddOutputUI.kt
+++ src/main/kotlin/ui/outputs/AddOutputUI.kt
@@ -2,15 +2,22 @@ package ui.outputs
 
 import ui.common.Navigation
 import ui.common.UIForm
+import ui.common.UIInput
+import ui.common.chart.SimpleChart
 import java.awt.BorderLayout
 import java.awt.FlowLayout
 import javax.inject.Inject
+import javax.swing.Box
 import javax.swing.JButton
 import javax.swing.JComponent
 import javax.swing.JPanel
+import javax.swing.border.EmptyBorder
 
 class AddOutputUI : JPanel(), UIForm {
-    @Inject lateinit var navigation: Navigation
+    @Inject
+    lateinit var navigation: Navigation
+    private val chart by lazy { SimpleChart() }
+    private val outputName by lazy { UIInput("Output name") }
 
     init {
         setupUI()
@@ -19,10 +26,32 @@ class AddOutputUI : JPanel(), UIForm {
     private fun setupUI() {
         layout = BorderLayout()
         add(bottomUI(), BorderLayout.SOUTH)
+        add(centerUI(), BorderLayout.CENTER)
+        add(topUI(), BorderLayout.NORTH)
     }
 
     override fun saveData() {
         println("Saving output...")
+    }
+
+    private fun topUI(): JComponent {
+        val ui = Box.createHorizontalBox()
+
+        ui.add(outputName)
+
+        return ui.apply {
+            border = EmptyBorder(10, 10, 10, 10)
+        }
+    }
+
+    private fun centerUI(): JComponent {
+        val ui = JPanel()
+
+        ui.add(chart)
+
+        return ui.apply {
+            border = EmptyBorder(10, 10, 10, 10)
+        }
     }
 
     private fun bottomUI(): JComponent {
