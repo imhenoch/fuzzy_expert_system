3498fd06d14f2534990b3724a5f720bd65cd4bc3
Isidro Henoch <isidro.henoch@gmail.com>
2018-10-14 23:39:13 -0500

    Adds the piece of code that creates all the intersections between variable labels

--- src/main/kotlin/fuzzy/FAM.kt
+++ src/main/kotlin/fuzzy/FAM.kt
@@ -1,12 +1,36 @@
 package fuzzy
 
 import files.Register
+import models.FAMCell
 import models.Variable
 
-fun generateFAM(variables: ArrayList<Register<Variable>>): ArrayList<Pair<Array<Int>, Int>> {
-    val FAM = ArrayList<Pair<Array<Int>, Int>>()
+fun generateFAM(variables: ArrayList<Register<Variable>>): ArrayList<FAMCell> {
+    val FAM = ArrayList<FAMCell>()
 
+    val limiters = IntArray(variables.size) { i ->
+        variables[i].data!!.labels.size
+    }
+    val counters = IntArray(variables.size) { 0 }
 
+    while (true) {
+        if (counters[0] == limiters[0])
+            break
+
+        val antecedents = IntArray(variables.size) { i -> counters[i] }
+        FAM.add(FAMCell(antecedents, 0))
+
+        incrementCounters(counters, limiters, counters.lastIndex)
+    }
 
     return FAM
+}
+
+private fun incrementCounters(counters: IntArray, limiters: IntArray, index: Int) {
+    counters[index] += 1
+    if (counters[index] == limiters[index]) {
+        if (index != 0) {
+            counters[index] = 0
+            incrementCounters(counters, limiters, index - 1)
+        }
+    }
 }
