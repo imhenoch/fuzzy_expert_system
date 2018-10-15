286e3ec86e89a28d65537e688d83cba39944a88d
Isidro Henoch <isidro.henoch@gmail.com>
2018-10-15 02:39:21 -0500

    Changes the L&F

--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -1,7 +1,9 @@
 package main
 
 import ui.ParentWindow
+import javax.swing.UIManager
 
 fun main(args: Array<String>) {
+    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
     ParentWindow()
 }
