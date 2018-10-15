b6d3e8365746af2b732285d152c9e3cc995c5129
Isidro Henoch <isidro.henoch@gmail.com>
2018-10-14 21:16:23 -0500

    Minor UI changes to the ResultUI object that allows the user to return to the home screen

--- src/main/kotlin/ui/result/ResultUI.kt
+++ src/main/kotlin/ui/result/ResultUI.kt
@@ -1,7 +1,35 @@
 package ui.result
 
+import ui.common.Navigation
+import java.awt.BorderLayout
+import java.awt.FlowLayout
+import javax.inject.Inject
+import javax.swing.JButton
+import javax.swing.JComponent
 import javax.swing.JPanel
 
 class ResultUI : JPanel() {
+    @Inject
+    lateinit var navigation: Navigation
 
+    init {
+        setupUI()
+    }
+
+    private fun setupUI() {
+        layout = BorderLayout()
+        add(bottomUI(), BorderLayout.SOUTH)
+    }
+
+    private fun bottomUI(): JComponent {
+        val ui = JPanel(FlowLayout(FlowLayout.RIGHT, 10, 10))
+
+        ui.add(JButton("Back to home").apply {
+            addActionListener {
+                navigation.navigateToHome()
+            }
+        })
+
+        return ui
+    }
 }
