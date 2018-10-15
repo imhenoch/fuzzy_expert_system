311607ca908dcb7a239abdf00eb456c0fbc0e77c
Isidro Henoch <isidro.henoch@gmail.com>
2018-10-15 08:12:05 -0500

    Adds a field to enter the username

--- src/main/kotlin/ui/inference/ResultUI.kt
+++ src/main/kotlin/ui/inference/ResultUI.kt
@@ -7,6 +7,9 @@ import fuzzy.generateFAM
 import models.Output
 import models.Range
 import models.Variable
+import org.knowm.xchart.XChartPanel
+import org.knowm.xchart.XYChart
+import org.knowm.xchart.XYChartBuilder
 import ui.common.Navigation
 import ui.common.UIContainer
 import ui.questions.QuestionsUI
@@ -16,6 +19,7 @@ import javax.inject.Inject
 import javax.swing.JButton
 import javax.swing.JComponent
 import javax.swing.JPanel
+import javax.swing.border.EmptyBorder
 
 class ResultUI : JPanel(), UIContainer {
     @Inject
@@ -23,6 +27,7 @@ class ResultUI : JPanel(), UIContainer {
     private lateinit var variables: ArrayList<Register<Variable>>
     private lateinit var ranges: ArrayList<Register<Range>>
     private lateinit var outputs: ArrayList<Register<Output>>
+    private lateinit var chartUI: XYChart
 
     init {
         setupUI()
@@ -30,7 +35,23 @@ class ResultUI : JPanel(), UIContainer {
 
     private fun setupUI() {
         layout = BorderLayout()
+        add(centerUI(), BorderLayout.CENTER)
         add(bottomUI(), BorderLayout.SOUTH)
+    }
+
+    private fun centerUI(): JComponent {
+        chartUI = XYChartBuilder().title(QuestionsUI.USER).build()
+        chartUI.styler.apply {
+            xAxisMin = 0.0
+            yAxisMin = 0.0
+            xAxisMax = 100.0
+            yAxisMax = 1.0
+        }
+
+        val ui = XChartPanel<XYChart>(chartUI)
+        return ui.apply {
+            border = EmptyBorder(30, 30, 30, 30)
+        }
     }
 
     private fun bottomUI(): JComponent {
@@ -39,6 +60,7 @@ class ResultUI : JPanel(), UIContainer {
         ui.add(JButton("Back to home").apply {
             addActionListener {
                 QuestionsUI.ANSWERS.clear()
+                QuestionsUI.USER = ""
                 navigation.navigateToHome()
             }
         })
--- src/main/kotlin/ui/questions/QuestionsUI.kt
+++ src/main/kotlin/ui/questions/QuestionsUI.kt
@@ -4,20 +4,24 @@ import models.Answer
 import models.Variable
 import ui.common.Navigation
 import ui.common.UIContainer
+import ui.common.UIInput
 import java.awt.BorderLayout
 import javax.inject.Inject
 import javax.swing.JPanel
 import javax.swing.SwingUtilities
+import javax.swing.border.EmptyBorder
 
 class QuestionsUI : JPanel(), UIContainer {
     companion object {
         val ANSWERS = ArrayList<Answer>()
+        var USER = ""
     }
 
     @Inject
     lateinit var navigation: Navigation
     private val variables by lazy { ArrayList<Variable>() }
     private val answers by lazy { ArrayList<Answer>() }
+    private val userUI by lazy { UIInput("Username") }
     private var actualVariable = 0
 
     init {
@@ -26,7 +30,11 @@ class QuestionsUI : JPanel(), UIContainer {
 
     private fun setupUI() {
         layout = BorderLayout()
+        add(topUI(), BorderLayout.NORTH)
+        border = EmptyBorder(10, 10, 10, 10)
     }
+
+    private fun topUI() = userUI
 
     override fun fetchData() {
         Variable().fetch().forEach { v ->
@@ -39,7 +47,6 @@ class QuestionsUI : JPanel(), UIContainer {
     }
 
     private fun generateNewQuestion() {
-        removeAll()
         add(SimpleQuestionUI(variables[actualVariable], this::nextQuestion), BorderLayout.CENTER)
         SwingUtilities.updateComponentTreeUI(this)
     }
@@ -49,12 +56,15 @@ class QuestionsUI : JPanel(), UIContainer {
         actualVariable++
         if (actualVariable == variables.size)
             finishQuestions()
-        else
+        else {
+            remove(1)
             generateNewQuestion()
+        }
     }
 
     private fun finishQuestions() {
         ANSWERS.addAll(answers)
+        USER = userUI.text
         navigation.navigateToResult()
     }
 }
